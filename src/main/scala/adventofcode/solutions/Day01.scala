ae308f2772dc34d0822899df14038e6d28637dfe
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-01 06:58:12 +0100

    Improved solution 1

--- src/main/scala/adventofcode/solutions/Day01.scala
+++ src/main/scala/adventofcode/solutions/Day01.scala
@@ -2,27 +2,33 @@ package adventofcode.solutions
 
 import adventofcode.Day
 
+import scala.collection.Searching._
+
 object Day01 extends Day(1):
 
   val goal = 2020
   val ns = lines.map(_.toInt).sorted
   
   def pair(a: Int, b: Int): Int =
-    ns(a) + ns(b) match
-      case `goal` => ns(a) * ns(b)
-      case s if s < goal => pair(a + 1, b)
-      case s if s > goal => pair(a, b - 1)
-  
+    val sum = ns(a) + ns(b)
+    if sum == goal then
+      ns(a) * ns(b)
+    else if sum < goal then
+      pair(a + 1, b)
+    else
+      pair(a, b - 1)
+
   override def solutionA = pair(0, ns.size - 1)
 
-  def triple: Int =
-    (for {
-      a <- ns.indices
-      b <- ns.indices.drop(a + 1)
-      c <- ns.indices.drop(b + 1)
-      vs = Seq(a, b, c).map(ns)
-      if vs.sum == goal
-    } yield vs.product).head
-  
-  override def solutionB = triple
+  def triple(a: Int, b: Int): Int =
+    if b == ns.size - 1 then
+      triple(a + 1, a + 2)
+    else if ns(a) + ns(b) > goal then
+      triple(a, b + 1)
+    else
+      val sum = ns(a) + ns(b)
+      ns.view.slice(b + 1, ns.size).map(_ + sum).search(goal) match
+        case Found(i) => Seq(a, b, i + b + 1).map(ns).product
+        case _ => triple(a, b + 1)
 
+  override def solutionB = triple(0, 1)
