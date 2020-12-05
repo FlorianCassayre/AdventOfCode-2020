e14f256e885bc9b0ce9a59df958c6ab4859fef7f
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-05 07:12:34 +0100

    Improved day 5

--- src/main/scala/adventofcode/solutions/Day05.scala
+++ src/main/scala/adventofcode/solutions/Day05.scala
@@ -4,36 +4,35 @@ import adventofcode.Day
 
 object Day05 extends Day(5):
 
-  case class Acc(c1: Int, c2: Int, r1: Int, r2: Int)
-  case class Seat(c: Int, r: Int):
-    val id = c * 8 + r
-  
-  val (rows, columns) = (128, 8)
-  
-  def toSeat(s: String): Seat =
-    val result = s.foldLeft(Acc(0, rows, 0, columns)) { case (acc@Acc(c1, c2, r1, r2), c) =>
-      val (cr, rr) = ((c2 - c1) / 2, (r2 - r1) / 2)
-      c match
-        case 'F' => acc.copy(c2 = c2 - cr)
-        case 'B' => acc.copy(c1 = c1 + cr)
-        case 'L' => acc.copy(r2 = r2 - rr)
-        case 'R' => acc.copy(r1 = r1 + rr)
+  case class Seat(r: Int, c: Int):
+    val id = r * 8 + c
+
+  val (rows, columns) = (0 until 128, 0 until 8)
+
+  def toSeat(code: String): Seat =
+    val (Seq(r), Seq(c)) = code.foldLeft(rows, columns) { case ((r, c), h) =>
+      val (rh, ch) = (r.size / 2, c.size / 2)
+      h match
+        case 'F' => (r.dropRight(rh), c)
+        case 'B' => (r.drop(rh), c)
+        case 'L' => (r, c.dropRight(ch))
+        case 'R' => (r, c.drop(ch))
     }
-    Seat(result.c1, result.r1)
+    Seat(r, c)
 
   val seats = lines.map(toSeat)
 
   override def solutionA = seats.map(_.id).max
 
-  val seatsIds = seats.map(_.id)
+  val seatsIds = seats.map(_.id).toSet
 
-  val empty =
+  val found =
     for
-    r <- (1 until (rows - 1))
-    c <- (0 until columns)
+    r <- rows.drop(1).dropRight(1)
+    c <- columns
     seat = Seat(r, c)
     if !seatsIds.contains(seat.id)
-    if seatsIds.contains(seat.id - 1) && seatsIds.contains(seat.id + 1) 
+    if seatsIds.contains(seat.id - 1) && seatsIds.contains(seat.id + 1)
     yield seat
-  
-  override def solutionB = empty.head.id
+
+  override def solutionB = found.head.id
