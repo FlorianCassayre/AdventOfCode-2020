1341f0cf39d36824037c4ca96e46c198ab1a3399
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-06 09:20:16 +0100

    Improved day 5 (bis)

--- src/main/scala/adventofcode/solutions/Day05.scala
+++ src/main/scala/adventofcode/solutions/Day05.scala
@@ -3,36 +3,18 @@ package adventofcode.solutions
 import adventofcode.Day
 
 object Day05 extends Day(5):
+  
+  def parse(`0`: Char, `1`: Char)(code: String): Int =
+    code.map {
+      case `0` => 0
+      case `1` => 1
+    }.foldLeft(0)(_ << 1 | _)
 
-  case class Seat(r: Int, c: Int):
-    val id = r * 8 + c
+  val seats = lines
+    .map(_.splitAt(7))
+    .map((r, c) => parse('F', 'B')(r) * 8 + parse('L', 'R')(c))
+    .sorted
 
-  val (rows, columns) = (0 until 128, 0 until 8)
-
-  def toSeat(code: String): Seat =
-    val (Seq(r), Seq(c)) = code.foldLeft(rows, columns) { case ((r, c), h) =>
-      val (rh, ch) = (r.size / 2, c.size / 2)
-      h match
-        case 'F' => (r.dropRight(rh), c)
-        case 'B' => (r.drop(rh), c)
-        case 'L' => (r, c.dropRight(ch))
-        case 'R' => (r, c.drop(ch))
-    }
-    Seat(r, c)
-
-  val seats = lines.map(toSeat)
-
-  override def solutionA = seats.map(_.id).max
-
-  val seatsIds = seats.map(_.id).toSet
-
-  val found =
-    for
-    r <- rows.drop(1).dropRight(1)
-    c <- columns
-    seat = Seat(r, c)
-    if !seatsIds.contains(seat.id)
-    if seatsIds.contains(seat.id - 1) && seatsIds.contains(seat.id + 1)
-    yield seat
-
-  override def solutionB = found.head.id
+  override def solutionA = seats.max
+  
+  override def solutionB = seats.tail.zip(seats).find(_ - _ == 2).map(_._1 - 1).get
