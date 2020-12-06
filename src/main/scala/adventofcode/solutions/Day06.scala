9f5b5d060eee18d4cab9c512182ab0cd4e8d74ce
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-06 06:20:10 +0100

    Improved day 6

--- src/main/scala/adventofcode/solutions/Day06.scala
+++ src/main/scala/adventofcode/solutions/Day06.scala
@@ -6,6 +6,6 @@ object Day06 extends Day(6):
 
   val groups = input.split(lineSeparator + lineSeparator).map(_.split(lineSeparator))
 
-  override def solutionA = groups.map(_.flatten.toSet.size).sum
+  override def solutionA = groups.map(_.flatten.distinct.size).sum
 
-  override def solutionB = groups.map(g => g.flatten.groupBy(identity).filter(_._2.size == g.size).size).sum
+  override def solutionB = groups.map(g => g.flatten.distinct.count(c => g.forall(_.contains(c)))).sum
