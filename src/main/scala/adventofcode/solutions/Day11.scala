de5a1aaeef3d992450e4106ccc938a1a393b641d
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-11 07:09:31 +0100

    Improved day 11

--- src/main/scala/adventofcode/solutions/Day11.scala
+++ src/main/scala/adventofcode/solutions/Day11.scala
@@ -4,48 +4,37 @@ import adventofcode.Day
 
 object Day11 extends Day(11):
 
-  case class Point(x: Int, y: Int) {
+  case class Point(x: Int, y: Int):
     def +(that: Point): Point = Point(x + that.x, y + that.y)
-  }
 
   val map = lines.map(_.map {
     case 'L' => Some(false)
     case '.' => None
   })
 
-  type Board = IndexedSeq[IndexedSeq[Option[Boolean]]]
-
   def inBounds(p: Point): Boolean = p.x >= 0 && p.x < map.head.size && p.y >= 0 && p.y < map.size
 
   val range = -1 to 1
   val directions = range.flatMap(x => range.map(y => Point(x, y)).filter(_ != Point(0, 0)))
 
-  def pr(o: Option[Boolean]): Char = o match
-    case Some(false) => 'L'
-    case Some(true) => '#'
-    case None => '.'
-
-  def fixedPoint(f: Board => (Boolean, Point) => Boolean): Int =
-    def iterate(state: Board): Int =
-      val next: Board = state.zipWithIndex.map((row, y) => row.zipWithIndex.map((e, x) => e.map(v => f(state)(v, Point(x, y)))))
+  def fixedPoint(f: (Point => Option[Boolean]) => (Boolean, Point) => Boolean): Int =
+    def iterate(state: IndexedSeq[IndexedSeq[Option[Boolean]]]): Int =
+      val next = state.zipWithIndex.map((row, y) => row.zipWithIndex.map(
+        (e, x) => e.map(v => f(p => state(p.y)(p.x))(v, Point(x, y))))
+      )
       if next == state then state.flatten.count(_.exists(identity)) else iterate(next)
     iterate(map)
 
-  override def solutionA = fixedPoint { board => (e, p) =>
-    val occupied = directions.map(p + _).filter(inBounds).count(p => board(p.y)(p.x).exists(identity))
-    if e then (occupied < 4) else (occupied == 0)
+  override def solutionA = fixedPoint { f => (e, p) =>
+    val occupied = directions.map(p + _).filter(inBounds).map(f).count(_.exists(identity))
+    if e then occupied < 4 else occupied == 0
   }
 
   def scan(f: Point => Option[Boolean])(p: Point, direction: Point): Boolean =
     val next = p + direction
-    if inBounds(next) then
-      f(next) match
-        case Some(v) => v
-        case None => scan(f)(next, direction)
-    else
-      false
+    inBounds(next) && f(next).getOrElse(scan(f)(next, direction))
 
-  override def solutionB = fixedPoint { board => (e, p) =>
-    val occupied = directions.count(d => scan(pt => board(pt.y)(pt.x))(p, d))
-    if e then (occupied < 5) else (occupied == 0)
+  override def solutionB = fixedPoint { f => (e, p) =>
+    val occupied = directions.count(d => scan(f)(p, d))
+    if e then occupied < 5 else occupied == 0
   }
