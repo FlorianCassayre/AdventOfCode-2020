d4b895d9d6ba05377c45d5f3287d2d303743f7c4
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-11 21:21:01 +0100

    Improved day 11 (bis)

--- src/main/scala/adventofcode/solutions/Day11.scala
+++ src/main/scala/adventofcode/solutions/Day11.scala
@@ -15,26 +15,21 @@ object Day11 extends Day(11):
   def inBounds(p: Point): Boolean = p.x >= 0 && p.x < map.head.size && p.y >= 0 && p.y < map.size
 
   val range = -1 to 1
-  val directions = range.flatMap(x => range.map(y => Point(x, y)).filter(_ != Point(0, 0)))
+  val directions = for y <- range; x <- range; if x != 0 || y != 0; yield Point(x, y)
 
-  def fixedPoint(f: (Point => Option[Boolean]) => (Boolean, Point) => Boolean): Int =
+  def fixedPoint(adjacency: (Point => Option[Boolean]) => (Point, Point) => Boolean, turnover: Int): Int =
     def iterate(state: IndexedSeq[IndexedSeq[Option[Boolean]]]): Int =
-      val next = state.zipWithIndex.map((row, y) => row.zipWithIndex.map(
-        (e, x) => e.map(v => f(p => state(p.y)(p.x))(v, Point(x, y))))
-      )
+      val next = state.zipWithIndex.map((row, y) => row.zipWithIndex.map { (e, x) =>
+        val p = Point(x, y)
+        val occupied = directions.count(d => adjacency(q => state(q.y)(q.x))(p + d, d))
+        e.map(if _ then occupied < turnover else occupied == 0)
+      })
       if next == state then state.flatten.count(_.exists(identity)) else iterate(next)
     iterate(map)
 
-  override def solutionA = fixedPoint { f => (e, p) =>
-    val occupied = directions.map(p + _).filter(inBounds).map(f).count(_.exists(identity))
-    if e then occupied < 4 else occupied == 0
-  }
+  override def solutionA = fixedPoint(f => (p, _) => inBounds(p) && f(p).exists(identity), 4)
 
   def scan(f: Point => Option[Boolean])(p: Point, direction: Point): Boolean =
-    val next = p + direction
-    inBounds(next) && f(next).getOrElse(scan(f)(next, direction))
+    inBounds(p) && f(p).getOrElse(scan(f)(p + direction, direction))
 
-  override def solutionB = fixedPoint { f => (e, p) =>
-    val occupied = directions.count(d => scan(f)(p, d))
-    if e then occupied < 5 else occupied == 0
-  }
+  override def solutionB = fixedPoint(scan, 5)
