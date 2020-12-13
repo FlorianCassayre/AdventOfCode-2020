e97b718a3841f5060b7f176aa03599d276302f4c
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-13 07:13:59 +0100

    Improved day 13

--- src/main/scala/adventofcode/solutions/Day13.scala
+++ src/main/scala/adventofcode/solutions/Day13.scala
@@ -12,18 +12,15 @@ object Day13 extends Day(13):
     .filter((b, _) => b != "x")
     .map((b, i) => (b.toLong, i.toLong))
 
-  def remainders(na: Seq[(Long, Long)]): Long =
-    val product = na.map(_._1).product
+  def remainder(na: Seq[(Long, Long)]): Long =
+    val product = na.map((n, _) => n).product
     def iterate(na: Seq[(Long, Long)], acc: Long): Long =
-      def inverse(a: Long, b: Long): Long =
-        def euclidean(a: Long, b: Long, x: Long, y: Long): Long =
-          if a > 1 then euclidean(b, a % b, y - (a / b) * x, x) else y
-        if b == 1 then 1 else euclidean(a, b, 0, 1).pipe(x => if (x < 0) x + b else x)
+      def euclidean(a: Long, b: Long, x: Long, y: Long): Long = if a > 1 then euclidean(b, a % b, y - (a / b) * x, x) else y
       na match
-        case (hn, ha) +: t => (product / hn).pipe(p => iterate(t, acc + ha * inverse(p, hn) * p))
+        case (hn, ha) +: t => (product / hn).pipe(p => iterate(t, acc + ha * euclidean(p, hn, 0, 1).pipe(x => if x < 0 then x + hn else x) * p))
         case _ => acc
     iterate(na, 0) % product
 
-  override def solutionA = buses.map(_._1).map(b => (b, b * (timestamp / b + 1))).minBy((_, t) => t).pipe((b, t) => b * (t - timestamp))
+  override def solutionA = buses.map((b, _) => (b, timestamp % b)).maxBy((_, t) => t).pipe((b, t) => b * (b - t))
 
-  override def solutionB = remainders(buses.map((b, i) => (b, (b - i))))
+  override def solutionB = remainder(buses.map((b, i) => (b, b - i)))
