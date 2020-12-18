a9daeb6b8960caeb909ef030376db764705acb82
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-18 14:02:46 +0100

    Improved day 18

--- src/main/scala/adventofcode/solutions/Day18.scala
+++ src/main/scala/adventofcode/solutions/Day18.scala
@@ -2,43 +2,52 @@ package adventofcode.solutions
 
 import adventofcode.Day
 
-import scala.util.Try
-
 object Day18 extends Day(18):
 
-  val (plus, times) = ('+', '*')
-  
-  val operations: Map[Char, (Long, Long) => Long] = Map(plus -> (_ + _), times -> (_ * _))
+  enum Token:
+    case Literal(v: Int)
+    case Plus
+    case Times
+    case OpenParenthesis
+    case CloseParenthesis
 
-  val allTokens = lines.map(_.replace(" ", "").toCharArray.toIndexedSeq)
+  import Token._
 
-  def evaluate(tokens: Seq[Char], precedences: Seq[Seq[Char]]): Long =
-    def precedence(op: Char): Int = precedences.indexWhere(_.contains(op))
-    def postfix(seq: Seq[Char], output: Seq[Char], stack: Seq[Char]): Seq[Char] =
+  val expressions = lines.map(_.toCharArray.toIndexedSeq.filter(_ != ' ').map {
+    case '(' => OpenParenthesis
+    case ')' => CloseParenthesis
+    case '+' => Plus
+    case '*' => Times
+    case d => Literal(d.asDigit)
+  })
+
+  def evaluate(precedences: Seq[Seq[Token]]): Long =
+    def precedence(op: Token): Int = precedences.indexWhere(_.contains(op))
+    def postfix(seq: Seq[Token], output: Seq[Token], stack: Seq[Token]): Seq[Token] =
       seq match
         case h +: tail =>
           h match
-            case _ if h.isDigit => postfix(tail, h +: output, stack)
-            case '(' =>
-              postfix(tail, output, h +: stack)
-            case ')' =>
-              val (left, right) = stack.span(_ != '(')
-              val newOutput = left.reverse ++ output
-              val newStack1 = if right.head == '(' then right.tail else right
-              postfix(tail, newOutput, newStack1)
+            case _: Literal => postfix(tail, h +: output, stack)
+            case OpenParenthesis => postfix(tail, output, h +: stack)
+            case CloseParenthesis =>
+              val (left, right) = stack.span(_ != OpenParenthesis)
+              postfix(tail, left.reverse ++ output, if right.head == OpenParenthesis then right.tail else right)
             case _ =>
-              val (left, right) = stack.span(c => c != '(' && precedence(c) >= precedence(h))
+              val (left, right) = stack.span(c => c != OpenParenthesis && precedence(c) >= precedence(h))
               postfix(tail, left.reverse ++ output, h +: right)
         case _ => stack.reverse ++ output
-    def reduce(seq: Seq[Char]): (Long, Int) =
+    def reduce(seq: Seq[Token]): (Long, Int) =
       seq.head match
-        case h if h.isDigit => (seq.head.asDigit, 1)
+        case Literal(v) => (v, 1)
         case op =>
           val (left, i) = reduce(seq.tail)
           val (right, j) = reduce(seq.tail.drop(i))
-          (operations(op)(left, right), i + j + 1)
-    reduce(postfix(tokens, Seq.empty, Seq.empty))._1
+          val f: (Long, Long) => Long = (op: @unchecked) match
+            case Plus => _ + _
+            case Times => _ * _
+          (f(left, right), i + j + 1)
+    expressions.map(expression => reduce(postfix(expression, Seq.empty, Seq.empty))._1).sum
 
-  override def solutionA = allTokens.map(tokens => evaluate(tokens, Seq(Seq(times, plus)))).sum
+  override def solutionA = evaluate(Seq(Seq(Times, Plus)))
 
-  override def solutionB = allTokens.map(tokens => evaluate(tokens, Seq(Seq(times), Seq(plus)))).sum
+  override def solutionB = evaluate(Seq(Seq(Times), Seq(Plus)))
