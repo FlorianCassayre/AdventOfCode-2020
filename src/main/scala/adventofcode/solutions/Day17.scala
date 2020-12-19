49a01ecc1825f912f8ca8e017de5db7913943ea1
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-19 21:26:00 +0100

    Improved day 17

--- src/main/scala/adventofcode/solutions/Day17.scala
+++ src/main/scala/adventofcode/solutions/Day17.scala
@@ -4,26 +4,22 @@ import adventofcode.Day
 
 object Day17 extends Day(17):
 
-  case class Cube(x: Int, y: Int, z: Int = 0, w: Int = 0):
-    def +(that: Cube): Cube = Cube(x + that.x, y + that.y, z + that.z, w + that.w)
-
   val space = lines.zipWithIndex.flatMap((rows, y) => rows.zipWithIndex.flatMap {
-    case ('#', x) => Some(Cube(x, y))
+    case ('#', x) => Some(Seq(x, y))
     case ('.', _) => None
   }).toSet
 
-  def simulate(fourDimensions: Boolean): Int =
-    val r = -1 to 1
-    val neighbours = for x <- r; y <- r; z <- r; w <- if fourDimensions then r else Seq(0); if Seq(x, y, z, w).exists(_ != 0) yield Cube(x, y, z, w)
-    val states = LazyList.unfold(space) { s =>
-      val next = s.flatMap(cube => neighbours.map(cube + _)).map { cube =>
-        val active = neighbours.count(n => s.contains(cube + n))
+  def simulate(dimensions: Int): Int =
+    val neighbours = Seq.fill(dimensions)(-1 to 1).flatten.combinations(dimensions).flatMap(_.permutations).filter(_.exists(_ != 0)).toSeq
+    val states = LazyList.unfold(space.map(_.padTo(dimensions, 0))) { s =>
+      val next = s.flatMap(cube => neighbours.map(_.zip(cube).map(_ + _))).map { cube =>
+        val active = neighbours.map(_.zip(cube).map(_ + _)).count(s.contains)
         if s.contains(cube) then cube -> (2 to 3).contains(active) else cube -> (active == 3)
       }.collect { case (cube, true) => cube }
       Some(next.size, next)
     }
     states(6 - 1)
 
-  override def solutionA = simulate(false)
+  override def solutionA = simulate(3)
 
-  override def solutionB = simulate(true)
+  override def solutionB = simulate(4)
