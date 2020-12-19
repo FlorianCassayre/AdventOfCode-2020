db0438c2dbcf2d2da98357c6d1311a79a35c1566
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-19 07:32:01 +0100

    Improved day 19

--- src/main/scala/adventofcode/solutions/Day19.scala
+++ src/main/scala/adventofcode/solutions/Day19.scala
@@ -4,7 +4,7 @@ import adventofcode.Day
 
 object Day19 extends Day(19):
 
-  val parts = input.split(lineSeparator + lineSeparator).toVector.map(_.split(lineSeparator).toVector)
+  val parts = input.split(lineSeparator + lineSeparator).map(_.split(lineSeparator))
 
   enum Case:
     case Literal(c: Char)
@@ -12,33 +12,32 @@ object Day19 extends Day(19):
 
   import Case._
 
-  val rules = parts.head.map { case s"$id: $rule" =>
-    val cse = rule match
-      case s""""$c"""" => Literal(c.head)
-      case other => Pipe(other.split(" \\| ").toSeq.map(_.split(" ").toSeq.map(_.toInt)))
-    id.toInt -> cse
-  }.toMap
+  def parse(s: String): (Int, Case) = s match
+    case s"$id: $rule" =>
+      val cse = rule match
+        case s""""$c"""" => Literal(c.head)
+        case other => Pipe(other.split(" \\| ").toSeq.map(_.split(" ").toSeq.map(_.toInt)))
+      id.toInt -> cse
 
-  val messages = parts.tail.head
+  val (rules, messages) = (parts.head.map(parse).toMap, parts.tail.head)
 
   val max = messages.map(_.size).max
 
-  val updatedRules  = rules + (8 -> Pipe(Seq(Seq(42), Seq(42, 8)))) + (11 -> Pipe(Seq(Seq(42, 31), Seq(42, 11, 31))))
-
-  def build(rules: Map[Int, Case], id: Int, depth: Int): String =
-    if depth >= max then
+  def build(rules: Map[Int, Case], id: Int = 0, length: Int = 0): String =
+    if length >= max then
       ""
     else
       rules(id) match
         case Literal(c) => c.toString
         case Pipe(rule) =>
-          val cases = rule.map(_.map(s => build(rules, s, depth + 1)).mkString).mkString("|")
-          s"(?:$cases)"
+          val cases = rule.map(r => r.map(s => build(rules, s, length + r.size)).mkString).mkString("|")
+          s"($cases)"
 
-  val regex1 = build(rules, 0, 0).r
+  def count(rules: Map[Int, Case]): Int = messages.count(build(rules).r.matches)
 
-  override def solutionA = messages.count(s => regex1.matches(s))
+  override def solutionA = count(rules)
 
-  val regex2 = build(updatedRules, 0, 0).r
+  val additionalRules = """8: 42 | 42 8
+                          |11: 42 31 | 42 11 31""".stripMargin.split(lineSeparator)
 
-  override def solutionB = messages.count(s => regex2.matches(s))
+  override def solutionB = count(rules ++ additionalRules.map(parse))
