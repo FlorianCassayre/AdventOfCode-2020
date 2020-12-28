d3a6305b825371f2bffa0a1868227425472cdfdb
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-28 23:06:47 +0100

    Improved day 25

--- src/main/scala/adventofcode/solutions/Day25.scala
+++ src/main/scala/adventofcode/solutions/Day25.scala
@@ -5,19 +5,9 @@ import adventofcode.Day
 object Day25 extends Day(25):
 
   val Seq(publicKey1, publicKey2) = lines.map(_.toInt).toSeq
-  
-  def transform(value: Int, subject: Int): Int = ((value.toLong * subject) % 20201227).toInt
 
-  def pow(v: Int, e: Int, acc: Int = 1): Int = if e > 0 then pow(v, e - 1, transform(acc, v)) else acc
+  def stream(subject: Long): LazyList[Long] = LazyList.unfold(1L)(v => Some(v, (v * subject) % 20201227))
 
-  def crack(public: Int, subject: Int = 7, loop: Int = 0, acc: Int = 1): Int =
-    if acc != public then
-      crack(public, subject, loop + 1, transform(acc, subject))
-    else
-      loop
-
-  val (loop1, loop2) = (crack(publicKey1), crack(publicKey2))
-
-  override def solutionA = pow(publicKey2, loop1)
+  override def solutionA = stream(7).zip(stream(publicKey2)).find((k, _) => k == publicKey1).get._2
 
   override def solutionB = ""
