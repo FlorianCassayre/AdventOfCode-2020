3322f162835147793d6399b186cd34e37d0db796
Florian Cassayre <florian.cassayre@gmail.com>
2021-01-01 15:41:53 +0100

    Improved day 23

--- src/main/scala/adventofcode/solutions/Day23.scala
+++ src/main/scala/adventofcode/solutions/Day23.scala
@@ -5,29 +5,19 @@ import adventofcode.Day
 object Day23 extends Day(23):
 
   val initialCups = input.toSeq.map(_.asDigit)
-  val initialFirstCup = initialCups.head
 
-  case class Ring(size: Int, left: Map[Int, Int], right: Map[Int, Int]):
-    def picked(current: Int): Seq[Int] = Seq.iterate(right(current), 3)(right)
-    def link(a: Int, b: Int): Ring = copy(left = left + (b -> a), right = right + (a -> b))
-    def sequence: Seq[Int] = Seq.iterate(right(1), size - 1)(right)
-
-  object Ring:
-    def apply(cups: Seq[Int]): Ring =
-      val plusOne = cups :+ cups.head
-      Ring(cups.size, plusOne.tail.zip(plusOne).toMap, plusOne.zip(plusOne.tail).toMap)
-
-  def play(turn: Int, current: Int, ring: Ring): Ring =
-    if turn > 0 then
+  def play(turns: Int, cups: Seq[Int]): Seq[Int] =
+    val size = cups.size
+    val (_, ring) = (0 until turns).foldLeft((cups.head, cups.zip(cups.tail :+ cups.head).toMap)) { case ((current, ring), _) =>
+      val picked = Seq.iterate(ring(current), 3)(ring)
       val stream = LazyList.iterate(current)(v => (v - 1 + ring.size - 1) % ring.size + 1).tail
-      val picked = ring.picked(current)
       val destination = stream.dropWhile(picked.contains).head
-      val newRing = ring.link(current, ring.right(picked.last)).link(destination, picked.head).link(picked.last, ring.right(destination))
-      val newCurrent = newRing.right(current)
-      play(turn - 1, newCurrent, newRing)
-    else
-      ring
+      val newRing = ring ++ Map(current -> ring(picked.last), destination -> picked.head, picked.last -> ring(destination))
+      val newCurrent = newRing(current)
+      (newCurrent, newRing)
+    }
+    Seq.iterate(ring(1), size - 1)(ring)
 
-  override def solutionA = play(100, initialFirstCup, Ring(initialCups)).sequence.mkString
+  override def solutionA = play(100, initialCups).mkString
 
-  override def solutionB = play(10000000, initialFirstCup, Ring(initialCups ++ (initialCups.max to 1000000).tail)).sequence.take(2).map(_.toLong).product
+  override def solutionB = play(10000000, initialCups ++ (initialCups.max + 1 to 1000000)).take(2).map(_.toLong).product
